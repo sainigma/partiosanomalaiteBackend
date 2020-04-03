03ac015f46fcc2005cb1e2cb72f0131873c9d527
sainigma <sainigma@gmail.com>
2020-04-03 14:55:11 +0300

    Preliminary work for socket comms

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const sockets = new WebSocket.Server({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
-  ws.send('uusi')
-  ws.on('message', function incoming(message) {
-    if( message === 'yk' ){
-      ws.send('yk ok')
-    }else ws.send('vastaanotettu')
+const transmitToSocket = (socket,type,message) => {
+  let transmission = {
+    type,
+    message,
+    callsign:socket.callsign
+  }
+  transmission = JSON.stringify(transmission)
+  socket.send(transmission)
+}
+
+const broadcastToFrequency = (message, type, frequency, ignore) => {
+  sockets.clients.forEach( socket => {
+    if( ( ignore === undefined || ignore !== socket ) && frequency === socket.frequency ){
+      console.log(socket.frequency)
+      transmitToSocket(socket,type,message)
+      socket.send(message)
+    }
+  })
+}
+
+sockets.on('connection', function connection(socket) {
+  socket.callsign = ''
+  socket.group = ''
+  socket.subgroup = ''
+  socket.frequency = 0
+
+  socket.on('message', function incoming(message) {
+    try{
+      const transmission = JSON.parse(message)
+    
+      switch( transmission.type ){
+        case 'handshake':
+          socket.frequency = transmission.frequency
+          broadcastToFrequency('yk', transmission.type, transmission.frequency, socket)
+          break
+      }
+  
+      if( transmission.type === 'handshake' ){
+        broadcastToFrequency('yk',socket)
+      }else socket.send('vastaanotettu')
+    }catch(error){
+      console.log(error)
+    }
     console.log('received: %s', message);
   });
 });
